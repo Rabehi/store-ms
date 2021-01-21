72aafcfc5ef60c0ad0f95eea2f035faf2c72f9c4
Rafa Bernardino <rafa.bh11@gmail.com>
2021-01-21 11:59:11 +0100

    Shopping microservice

---
+++ shopping-ms/src/main/java/rabehi/store/shoppingms/model/Category.java
@@ -0,0 +1,10 @@
+package rabehi.store.shoppingms.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private Long id;
+    private String name;
+
+}
---
+++ shopping-ms/src/main/java/rabehi/store/shoppingms/model/Region.java
@@ -0,0 +1,9 @@
+package rabehi.store.shoppingms.model;
+
+import lombok.Data;
+
+@Data
+public class Region {
+    private Long id;
+    private String name;
+}
